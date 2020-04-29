ab2f5d932ee011fa2a06b89ef5991307223c6345
VortexAI <s.c.a.beukers@student.tue.nl>
2020-04-29 14:41:54 +0200

    added coordinates filter

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import csv
+
+coordinates = {}
+with open('../Dataset/airports.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for line in reader:
+        coordinates[line["AIRPORT_ID"]] = line["LATITUDE"] + "," + line["LONGITUDE"]
+
+print(coordinates)
