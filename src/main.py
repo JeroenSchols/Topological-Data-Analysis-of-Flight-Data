7676d87af2be3c68ed4c918a583c28e254b2f961
VortexAI <s.c.a.beukers@student.tue.nl>
2020-05-02 16:31:34 +0200

    Made airports no longer random for add edge

--- src/main.py
+++ src/main.py
@@ -11,10 +11,29 @@ for i, airport in airports.iterrows():
     # currently choses arbitrary colors
     mv.add_vertex(airport, np.random.choice(range(256)))
 
+print(isinstance(pd.DataFrame,pd.Series))
+print(isinstance(pd.Series,pd.DataFrame))
+
 for i, flight in flights.iterrows():
     # @TODO should have source and target airports of flights
-    source = airports.sample()
-    target = airports.sample()
+
+    #get check if airpot id is contained multiple times
+    #if so, get the first aiport that belongs to the given id
+    #else get the unique airport related to the id
+    if isinstance(airports.loc[flight["OriginAirportID"]], pd.DataFrame):
+        for j, airport in airports.loc[flight["OriginAirportID"]].iterrows():
+            source = airport
+            break
+    else:
+        source = airports.loc[flight["OriginAirportID"]]
+
+    if isinstance(airports.loc[flight["DestAirportID"]], pd.DataFrame):
+        for j, airport in airports.loc[flight["DestAirportID"]].iterrows():
+            target = airport
+            break
+    else:
+        target = airports.loc[flight["DestAirportID"]]
+
     if i > 50:
         break
     mv.add_edge(source, target, np.random.choice(range(256)))
