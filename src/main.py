fc1faa1c22a77051d45b278969cb5fc9143db44b
Chess <34745596+Chessnl@users.noreply.github.com>
2020-05-03 14:45:57 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,40 +2,18 @@ from src.MapVisualizer import MapVisualizer
 import pandas as pd
 import numpy as np
 
-flights = pd.read_csv("../Dataset/2019-01.csv", usecols=["FlightDate", "DepTime", "OriginAirportID", "DestAirportID"])
-airports = pd.read_csv("../Dataset/airports.csv", usecols=["AIRPORT_ID", "AIRPORT", "LATITUDE", "LONGITUDE"], index_col=["AIRPORT_ID"])
+flights = pd.read_csv("Dataset/2019-01.csv", usecols=["FlightDate", "DepTime", "OriginAirportID", "DestAirportID"])
+airports = pd.read_csv("Dataset/airports.csv", usecols=["AIRPORT_ID", "AIRPORT", "LATITUDE", "LONGITUDE"]).drop_duplicates("AIRPORT_ID").set_index("AIRPORT_ID", drop=False)
 
 mv = MapVisualizer()
 
-for i, airport in airports.iterrows():
-    # currently choses arbitrary colors
-    mv.add_vertex(airport, np.random.choice(range(256)))
-
-print(isinstance(pd.DataFrame,pd.Series))
-print(isinstance(pd.Series,pd.DataFrame))
-
 for i, flight in flights.iterrows():
-    # @TODO should have source and target airports of flights
-
-    #get check if airpot id is contained multiple times
-    #if so, get the first aiport that belongs to the given id
-    #else get the unique airport related to the id
-    if isinstance(airports.loc[flight["OriginAirportID"]], pd.DataFrame):
-        for j, airport in airports.loc[flight["OriginAirportID"]].iterrows():
-            source = airport
-            break
-    else:
-        source = airports.loc[flight["OriginAirportID"]]
-
-    if isinstance(airports.loc[flight["DestAirportID"]], pd.DataFrame):
-        for j, airport in airports.loc[flight["DestAirportID"]].iterrows():
-            target = airport
-            break
-    else:
-        target = airports.loc[flight["DestAirportID"]]
-
-    if i > 50:
-        break
+    source = airports.loc[flight["OriginAirportID"]]
+    target = airports.loc[flight["DestAirportID"]]
+    mv.add_vertex(source, 0)
+    mv.add_vertex(target, 1)
     mv.add_edge(source, target, np.random.choice(range(256)))
+    if i % 1000 == 0:
+        print(i, len(flights))
 
 mv.open_display()
