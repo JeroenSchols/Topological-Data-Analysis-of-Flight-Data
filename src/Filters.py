a2470e09b0dccef916c7594cba7d4d524dd2a20a
ThomasDerwig <Thomas_Derwig@outlook.com>
2020-05-04 11:57:16 +0200

    Created Filter for dep and arr time
    Its ugly and gives some warnings but seems to work

--- src/Filters.py
+++ src/Filters.py
@@ -2,9 +2,33 @@ import pandas as pd
 
 
 # Filter flights based on start and end date
-def time_filter(flight, start, end):
+def day_filter(flight, start, end):
     flight['FlightDate'] = pd.to_datetime(flight['FlightDate'])
     print(flight.shape)
     filtered_flight = flight.query('@start <= FlightDate & FlightDate <= @end')
     print(filtered_flight.shape)
     return filtered_flight
+
+
+def time_filter(flight, start, end):
+    print(flight.shape)
+    #Do lots of stuff to get time format to be usable for DepTime
+    flight['DepTime'] = flight['DepTime'].astype(str).replace('\.0', '', regex=True)
+    flight = flight.query('DepTime!="nan"')
+    flight['DepTime'].replace({"2400": "0000"}, inplace=True)
+    flight['DepTime'] = flight['DepTime'].str.zfill(4)
+    flight['DepTime'] = flight['DepTime'].map(lambda a: a[:2] + ":" + a[2:])
+    flight['DepTime'] = pd.to_datetime(flight['DepTime'], format = '%H:%M')
+
+    #Same for ArrTime
+    flight['ArrTime'] = flight['ArrTime'].astype(str).replace('\.0', '', regex=True)
+    flight = flight.query('ArrTime!="nan"')
+    flight['ArrTime'].replace({"2400": "0000"}, inplace=True)
+    flight['ArrTime'] = flight['ArrTime'].str.zfill(4)
+    flight['ArrTime'] = flight['ArrTime'].map(lambda a: a[:2] + ":" + a[2:])
+    flight['ArrTime'] = pd.to_datetime(flight['ArrTime'], format = '%H:%M')
+
+    filtered_flight = flight.query('(@start <= ArrTime & ArrTime <= @end) | (@start <= DepTime & DepTime <= @end)')
+    print(filtered_flight.shape)
+    return filtered_flight
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 from src.MapVisualizer import MapVisualizer
-from src.Filters import time_filter
+from src.Filters import time_filter, day_filter
 import pandas as pd
 import numpy as np
 import datetime
 
-flights = pd.read_csv("Dataset/2019-01.csv", usecols=["FlightDate", "DepTime", "OriginAirportID", "DestAirportID"])
-airports = pd.read_csv("Dataset/airports.csv", usecols=["AIRPORT_ID", "AIRPORT", "LATITUDE", "LONGITUDE"]).drop_duplicates("AIRPORT_ID").set_index("AIRPORT_ID", drop=False)
+flights = pd.read_csv("../Dataset/2019-01.csv", usecols=["FlightDate", "DepTime", "ArrTime", "OriginAirportID", "DestAirportID"])
+airports = pd.read_csv("../Dataset/airports.csv", usecols=["AIRPORT_ID", "AIRPORT", "LATITUDE", "LONGITUDE"]).drop_duplicates("AIRPORT_ID").set_index("AIRPORT_ID", drop=False)
 
 mv = MapVisualizer()
 
-flights = time_filter(flights, datetime.datetime(2019, 1, 1), datetime.datetime(2019, 1, 10))
+flights = day_filter(flights, datetime.datetime(2019, 1, 1), datetime.datetime(2019, 1, 1))
+startTime = datetime.datetime(1900, 1, 1)
+flights = time_filter(flights, startTime.replace(hour=10, minute=00), startTime.replace(hour=12, minute=00))
+
 
 for i, flight in flights.iterrows():
     source = airports.loc[flight["OriginAirportID"]]
@@ -17,7 +20,5 @@ for i, flight in flights.iterrows():
     mv.add_vertex(source, 0)
     mv.add_vertex(target, 1)
     mv.add_edge(source, target, np.random.choice(range(256)))
-    if i > 10000:
-        break
 
 mv.open_display()
