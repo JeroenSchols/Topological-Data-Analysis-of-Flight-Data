3e01e4bde52a2bbd02d83de4c91db323133f2ae9
Chess <34745596+Chessnl@users.noreply.github.com>
2020-06-03 13:05:06 +0200

    plot cocycles

--- src/Utils.py
+++ src/Utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def merge_flights(flights):
@@ -21,3 +22,37 @@ def distance_matrix(flights, IDtoIndex):
     # Return results
     return res
 
+def drawLineColored(X, C):
+    for i in range(X.shape[0]-1):
+        plt.plot(X[i:i+2, 0], X[i:i+2, 1], c=C[i, :], lineWidth = 3)
+
+def plotCocycle2D(D, X, cocycle, thresh):
+    """
+    Given a 2D point cloud X, display a cocycle projected
+    onto edges under a given threshold "thresh"
+    """
+    #Plot all edges under the threshold
+    N = X.shape[0]
+    t = np.linspace(0, 1, 10)
+    c = plt.get_cmap('Greys')
+    C = c(np.array(np.round(np.linspace(0, 255, len(t))), dtype=np.int32))
+    C = C[:, 0:3]
+
+    for i in range(N):
+        for j in range(N):
+            if D[i, j] <= thresh:
+                Y = np.zeros((len(t), 2))
+                Y[:, 0] = X[i, 0] + t*(X[j, 0] - X[i, 0])
+                Y[:, 1] = X[i, 1] + t*(X[j, 1] - X[i, 1])
+                drawLineColored(Y, C)
+    #Plot cocycle projected to edges under the chosen threshold
+    for k in range(cocycle.shape[0]):
+        [i, j, val] = cocycle[k, :]
+        if D[i, j] <= thresh:
+            [i, j] = [min(i, j), max(i, j)]
+            a = 0.5*(X[i, :] + X[j, :])
+            plt.text(a[0], a[1], '%g'%val, color='b')
+    #Plot vertex labels
+    for i in range(N):
+        plt.text(X[i, 0], X[i, 1], '%i'%i, color='r')
+    plt.axis('equal')
--- src/main.py
+++ src/main.py
@@ -1,56 +1,45 @@
-from src.MapVisualizer import MapVisualizer
-from src.Graph_Based_Visualiser import GraphBasedVisualiser
 from src.Filters import *
 from src.Utils import *
 from ripser import ripser
 from persim import plot_diagrams
 import pandas as pd
-import numpy as np
 import datetime
+import tadasets
+from scipy.spatial.distance import cdist
+
 
 input_files = ["Dataset/" + str(y) + "-" + str(m+1).zfill(2) + ".csv" for m in range(1) for y in [2019]]
 frames = [pd.read_csv(file, usecols=["FlightDate", "DepTime", "ArrTime", "OriginAirportID", "DestAirportID"]) for file in input_files]
 flights = pd.concat(frames, ignore_index=True)
 
-airports = pd.read_csv("Dataset/airports.csv", usecols=["AIRPORT_ID", "AIRPORT", "LATITUDE", "LONGITUDE"])\
-    .drop_duplicates("AIRPORT_ID").set_index("AIRPORT_ID", drop=False)
-
-# mv = MapVisualizer()
-# gv = GraphBasedVisualiser()
+airports = pd.read_csv("Dataset/airports.csv", usecols=["AIRPORT_ID", "AIRPORT", "LATITUDE", "LONGITUDE"]).drop_duplicates("AIRPORT_ID").set_index("AIRPORT_ID", drop=False)
 
 flights = day_filter(flights, datetime.datetime(2019, 1, 1), datetime.datetime(2019, 1, 10))
 
-# startTime = datetime.datetime(1900, 1, 1)
-# flights = time_filter(flights, startTime.replace(hour=10, minute=00), startTime.replace(hour=12, minute=00))
-# ids = [14771, 12892]
-# flights = airport_filter(flights, ids)
-
-# flights = frame_to_frequency(flights, ["OriginAirportID", "DestAirportID"])
-# flights = frequency_filter(flights, 3)
-
 flights = merge_flights(flights)
 
-# dist = distance_matrix(flights)
 IDtoIndex = list(set(flights["OriginAirportID"].tolist()).union(flights["DestAirportID"].tolist()))
 IDtoIndex.sort()
 IDtoIndex = {val: key for key, val in enumerate(IDtoIndex)}
 
 dist_mat = distance_matrix(flights, IDtoIndex)
-dist_mat = (dist_mat.max() - dist_mat)
-dgms = ripser(dist_mat)['dgms']
-dgmsDM = ripser(dist_mat, distance_matrix=True)['dgms']
-plot_diagrams(dgms, show=True)
-plot_diagrams(dgmsDM, show=True)
+thresh = dist_mat.max()
+dist_mat = (thresh - dist_mat)
 
-# for (s, t) in flights:
-#     source = airports.loc[s]
-#     target = airports.loc[t]
-#
-#     gv.add_vertex(source, flights[(s, t)])
-#     gv.add_vertex(target, flights[(s, t)])
-#     gv.add_edge(source, target, flights[(s, t)])
-#     # if i > 10000:
-#     #     break
-#
-# gv.open_display()
-# mv.open_display()
+IDtoCoordinates = pd.DataFrame.from_dict(IDtoIndex, orient='index').reset_index(drop=False).merge(airports, left_on='index', right_index=True)[['LONGITUDE', 'LATITUDE']]
+coordinates = IDtoCoordinates.to_numpy()
+
+result = ripser(dist_mat, distance_matrix=True, do_cocycles=True, thresh=thresh-0.00001)
+
+cocycles = result['cocycles']
+
+diagrams = result['dgms']
+
+
+dgm1 = diagrams[1]
+idx = np.argmax(dgm1[:, 1] - dgm1[:, 0])
+cocycle = cocycles[1][idx]
+thresh = dgm1[idx, 1]-0.00001 #Project cocycle onto edges less than or equal to death time
+plotCocycle2D(dist_mat, coordinates, cocycle, thresh)
+plt.title("1-Form Thresh=%g"%thresh)
+plt.show()
