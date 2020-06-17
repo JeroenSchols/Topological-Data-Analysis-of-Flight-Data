0e1b32c0ea56c2bcb8d127a3e53c6e2ef76e9df4
VortexAI <s.c.a.beukers@student.tue.nl>
2020-06-17 15:46:25 +0200

    Added day/night functionality to the parser

--- src/Parser.py
+++ src/Parser.py
@@ -6,18 +6,21 @@ import re
 from datetime import date
 from src.parser_funcs import *
 
+zero_night = "Clusters/zero_day_night.txt"
+one_night = "Clusters/one_day_night.txt"
 zero_persistence_file = "Clusters/Zero persistence/res.txt"
 one_persistence_file = "Clusters/One persistence/res.txt"
 files = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 files_appended = ["Zero One persistence/"+file+".txt" for file in files]
 file_to_month = {file: month for num, (month, file) in enumerate(zip(files, files_appended))}
 month_to_num = {k: v for v, k in enumerate(calendar.month_name)}
-print(month_to_num)
+# print(month_to_num)
 default = {day:[] for day in calendar.day_name}
+default_night = {"day": [], "night": []}
 results = {}
 
 weekdays = calendar.day_name[:5]
-print(weekdays)
+# print(weekdays)
 
 # parse zero one data
 for file in files_appended:
@@ -41,6 +44,8 @@ result = copy.deepcopy(default)
 with open(zero_persistence_file) as file:
     prev_month = 0
     for line in file:
+        if line == "\n":
+            continue
         split = re.split(",|=|\n",line)
         month = int(split[1])
         day = int(split[3])
@@ -48,7 +53,7 @@ with open(zero_persistence_file) as file:
         if month == prev_month:
             day = calendar.day_name[date(2019, month, day).weekday()]
             result[day].append(clust)
-            results[month] = result
+            results2[month] = result
             prev_month = month
         else:
             result = copy.deepcopy(default)
@@ -57,7 +62,7 @@ with open(zero_persistence_file) as file:
             results2[month] = result
             prev_month = month
 
-results_to_latex(results, "zero_tabs")
+results_to_latex(results2, "zero_tabs")
 
 results3 = {}
 result = copy.deepcopy(default)
@@ -66,20 +71,14 @@ with open(one_persistence_file) as file:
     for line in file:
         if line == "\n":
             continue
-
-        if "Month" in line:
-            line = file.__next__()
-            month = int(line)
-            result = copy.deepcopy(default)
-            continue
-
         split = re.split(",|=|\n",line)
-        day = int(split[1])
-        clust = int(split[2])
+        month = int(split[1])
+        day = int(split[3])
+        clust = int(split[4])
         if month == prev_month:
             day = calendar.day_name[date(2019, month, day).weekday()]
             result[day].append(clust)
-            results[month] = result
+            results3[month] = result
             prev_month = month
         else:
             result = copy.deepcopy(default)
@@ -88,11 +87,160 @@ with open(one_persistence_file) as file:
             results3[month] = result
             prev_month = month
 
+results_to_latex(results3, "test")
+
+results = {}
+result = copy.deepcopy(default_night)
+with open(zero_night) as file:
+    prev_month_night = 0
+    prev_month_day = 0
+    for line in file:
+        if line == "\n":
+            continue
+        if "night" in line:
+            split = re.split(",|=|\n", line)
+            month = int(split[1])
+            clust = int(split[4])
+            if month == prev_month_night:
+                result["night"].append(clust)
+                results[month] = result
+                prev_month_night = month
+            else:
+                result = copy.deepcopy(default_night)
+                result["night"].append(clust)
+                results[month] = result
+                prev_month_night = month
+        else:
+            split = re.split(",|=|\n",line)
+            month = int(split[1])
+            clust = int(split[4])
+            if month == prev_month_day:
+                result["day"].append(clust)
+                results[month] = result
+                prev_month_day = month
+            else:
+                result = copy.deepcopy(default_night)
+                result["day"].append(clust)
+                results[month] = result
+                prev_month_day = month
+
+results_day_night_to_latex(results, "day_night_zero")
+
+results = {}
+result = copy.deepcopy(default_night)
+with open(one_night) as file:
+    prev_month_night = 0
+    prev_month_day = 0
+    for line in file:
+        if line == "\n":
+            continue
+        if "night" in line:
+            split = re.split(",|=|\n", line)
+            month = int(split[1])
+            clust = int(split[4])
+            if month == prev_month_night:
+                result["night"].append(clust)
+                results[month] = result
+                prev_month_night = month
+            else:
+                result = copy.deepcopy(default_night)
+                result["night"].append(clust)
+                results[month] = result
+                prev_month_night = month
+        else:
+            split = re.split(",|=|\n",line)
+            month = int(split[1])
+            clust = int(split[4])
+            if month == prev_month_day:
+                result["day"].append(clust)
+                results[month] = result
+                prev_month_day = month
+            else:
+                result = copy.deepcopy(default_night)
+                result["day"].append(clust)
+                results[month] = result
+                prev_month_day = month
+
+results_day_night_to_latex(results, "day_night_one")
+
+
+# results_day = {}
+# result_day = copy.deepcopy(default)
+# results_night = {}
+# result_night = copy.deepcopy(default)
+# with open(one_night) as file:
+#     prev_month_night = 0
+#     prev_month_day = 0
+#     for line in file:
+#         if line == "\n":
+#             continue
+#         if "night" in line:
+#             split = re.split(",|=|\n", line)
+#             month = int(split[1])
+#             day = int(split[3])
+#             clust = int(split[4])
+#             if month == prev_month_night:
+#                 day = calendar.day_name[date(2019, month, day).weekday()]
+#                 result_night[day].append(clust)
+#                 results_night[month] = result_night
+#                 prev_month_night = month
+#             else:
+#                 result_night = copy.deepcopy(default)
+#                 day = calendar.day_name[date(2019, month, day).weekday()]
+#                 result_night[day].append(clust)
+#                 results_night[month] = result_night
+#                 prev_month_night = month
+#         else:
+#             split = re.split(",|=|\n",line)
+#             month = int(split[1])
+#             day = int(split[3])
+#             clust = int(split[4])
+#             if month == prev_month_day:
+#                 day = calendar.day_name[date(2019, month, day).weekday()]
+#                 result_day[day].append(clust)
+#                 results_day[month] = result_day
+#                 prev_month_day = month
+#             else:
+#                 result_day = copy.deepcopy(default)
+#                 day = calendar.day_name[date(2019, month, day).weekday()]
+#                 result_day[day].append(clust)
+#                 results_day[month] = result_day
+#                 prev_month_day = month
+#
+# results_to_latex(results_day, "day_weeks_one")
+# results_to_latex(results_night, "night_weeks_one")
 
 
 
-print(results)
-results_to_latex(results, "one_tabs")
+# results_to_latex(results_day, "day_weeks_one")
+# results_to_latex(results_night, "night_weeks_one")
+
+# with open(one_persistence_file) as file:
+#     prev_month = 0
+#     for line in file:
+#         if line == "\n":
+#             continue
+#
+#         if "Month" in line:
+#             line = file.__next__()
+#             month = int(line)
+#             result = copy.deepcopy(default)
+#             continue
+#
+#         split = re.split(",|=|\n",line)
+#         day = int(split[1])
+#         clust = int(split[2])
+#         if month == prev_month:
+#             day = calendar.day_name[date(2019, month, day).weekday()]
+#             result[day].append(clust)
+#             results[month] = result
+#             prev_month = month
+#         else:
+#             result = copy.deepcopy(default)
+#             day = calendar.day_name[date(2019, month, day).weekday()]
+#             result[day].append(clust)
+#             results3[month] = result
+#             prev_month = month
 
 
 
--- src/parser_funcs.py
+++ src/parser_funcs.py
@@ -5,8 +5,51 @@ import copy
 import re
 from datetime import date
 
+
+
 month_to_num = {k: v for v, k in enumerate(calendar.month_name)}
 weekdays = calendar.day_name[:5]
+
+def results_day_night_to_latex(results, output_name):
+    sum_clust = {}
+    for month, times in results.items():
+        max = np.max([np.max(x) for _, x in times.items()])
+        summed = {}
+        if (max == -1):
+            continue
+        for time, clusts in times.items():
+            day_sum = [0] * (max + 1)
+            for clust in clusts:
+                day_sum[clust] += 1
+            summed[time] = day_sum
+        sum_clust[month] = pd.DataFrame.from_dict(summed)
+
+    final = None
+    with open(f"Clusters/{output_name}.tex", "w") as f:
+        for month, times in sum_clust.items():
+            if len(times.index) == 0:
+                continue
+            cluster_amount = times.index[-1]
+            tups = []
+            day_night = [0] * (cluster_amount + 1)
+
+            for key, val in times.iteritems():
+                temp = [x for x in val]
+                tups.append(str(temp))
+
+            tups = tups
+            columns = list(times.columns)
+
+            if final is None:
+                final = pd.DataFrame(np.array(tups).reshape((1, 2)), columns=columns,
+                                     index=[calendar.month_name[month]])
+            else:
+                final = final.append(
+                    pd.DataFrame(np.array(tups).reshape((1, 2)), columns=columns, index=[calendar.month_name[month]]))
+        f.write(final.to_latex())
+
+
+
 
 def results_to_latex(results, output_name):
     """
@@ -16,10 +59,13 @@ def results_to_latex(results, output_name):
     results_sum_clust = {}
     for file, result in results.items():
         max = np.max([np.max(x) for _, x in result.items()])
+        if (max == -1):
+            continue
         day_sum = {}
         for day, clusters in result.items():
             day_sum[day] = [0] * (max + 1)
             for cluster in clusters:
+
                 day_sum[day][cluster] += 1
         results_sum_clust[file] = pd.DataFrame.from_dict(day_sum)
 
@@ -27,6 +73,8 @@ def results_to_latex(results, output_name):
     summed = None
     with open(f"Clusters/{output_name}.tex", "w") as f:
         for month, results in results_sum_clust.items():
+            if len(results.index) == 0:
+                continue
             cluster_amount = results.index[-1]
             tups = []
             weekday = [0]*(cluster_amount+1)
@@ -45,9 +93,9 @@ def results_to_latex(results, output_name):
 
             if final is None:
                 final = pd.DataFrame(np.array(tups).reshape((1,7)), columns = columns, index = [calendar.month_name[month]])
-                summed = pd.DataFrame(np.array([str(weekday),str(weekend)]).reshape((1,2)), columns = ["Weekdays", "Weekends"], index = [month])
+                summed = pd.DataFrame(np.array([str(weekday),str(weekend)]).reshape((1,2)), columns = ["Weekdays", "Weekends"], index = [calendar.month_name[month]])
             else:
                 final = final.append(pd.DataFrame(np.array(tups).reshape((1,7)), columns = columns, index = [calendar.month_name[month]]))
-                summed = summed.append(pd.DataFrame(np.array([str(weekday), str(weekend)]).reshape((1, 2)),columns=["Weekdays", "Weekends"], index=[month]))
+                summed = summed.append(pd.DataFrame(np.array([str(weekday), str(weekend)]).reshape((1, 2)),columns=["Weekdays", "Weekends"], index=[calendar.month_name[month]]))
         f.write(final.to_latex())
         f.write(summed.to_latex())
